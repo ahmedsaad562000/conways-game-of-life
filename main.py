e88ee4950275c6f64d20e4cfba2e49bb7cecc413
DESKTOP-M5TOENG\AHMED <ahmed.moh.saad.hussein.ali@gmail.com>
2024-06-26 18:03:43 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+pygame.init()
+
+
+BLACK = (0, 0, 0)
+
+GREY = (128, 128, 128)
+
+PURPLE = (128, 0, 128)
+
+WIDTH, HEIGHT = 800, 800
+
+TILE_SIZE = 20
+
+GRID_WIDTH = WIDTH/TILE_SIZE
+
+GRID_HEIGHT = HEIGHT/TILE_SIZE
+
+FPS = 60
+screen = pygame.dis
